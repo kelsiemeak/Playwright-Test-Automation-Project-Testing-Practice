1367fec0e65142352631887f95e9da7cf767371f
Kelsie <kelsieblue2000@gmail.com>
2026-04-29 15:59:19 +0100

    Enhance login tests with unsuccessful login case
    Added tests for unsuccessful login and improved visibility checks.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-//navigate to website needs to be a beforeEach function
-
 test.describe('Login Tests', () => {
 test.beforeEach(async ({page}) => {
     await page.goto('https://practice.expandtesting.com/');
@@ -14,11 +12,32 @@ const passwordInput = page.getByRole('textbox', { name: 'Password' });
 
   await expect(page.locator('#username')).toBeVisible();
   await expect(usernameInput).toBeVisible();    
-  await (usernameInput).fill('practice');
+  await usernameInput.fill('practice');
   await expect(page.locator('#password')).toBeVisible();
   await expect(passwordInput).toBeVisible();
-  await (passwordInput).fill('SuperSecretPassword!');
+  await passwordInput.fill('SuperSecretPassword!');
+  await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
   await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
   await page.getByRole('link', { name: 'Logout' }).click();
+});
+
+
+test('Unsuccessful Login', async ({page}) => {
+    const usernameInput = page.getByRole('textbox', { name: 'Username' });
+    const passwordInput = page.getByRole('textbox', { name: 'Password' });
+    
+    await expect(page.locator('#username')).toBeVisible();
+    await expect(usernameInput).toBeVisible();    
+    await usernameInput.fill('practic');
+    await expect(page.locator('#password')).toBeVisible();
+    await expect(passwordInput).toBeVisible();
+    await passwordInput.fill('SuperSecretPassword');
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    await page.getByRole('button', { name: 'Login' }).click();
+    
+    await expect(page.locator('#flash').getByText('Your password is invalid!')).toBeVisible();
+    await page.getByRole('button', { name: 'Close', exact: true }).click();
+    await expect(page.getByRole('button', { name: 'Close'})).toBeHidden();
 });
 });
