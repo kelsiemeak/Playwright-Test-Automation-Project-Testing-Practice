77e1a9cb1adedaacd6bbc73f4db63a1c25aa9696
Kelsie <kelsieblue2000@gmail.com>
2026-05-09 17:15:12 +0100

    Enhance NavigationPage with login and file upload methods
    Added methods for successful login and file upload navigation.

--- page-object-model/navigation-page.ts
+++ page-object-model/navigation-page.ts
@@ -2,24 +2,29 @@ import {Page} from '@playwright/test';
 
 export class NavigationPage {
   readonly page: Page
+  readonly loginPageButton: Locator;
+  readonly loginButton: Locator;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+
 
   constructor (page: Page) {
     this.page = page
+    this.loginPageButton = this.page.getByRole('link', { name: 'Try it out' }).nth(1)
+    this.loginButton = this.page.getByRole('button', { name: 'Login' })
+    this.usernameInput = this.page.getByRole('textbox', { name: 'Username' });
+    this.passwordInput = this.page.getByRole('textbox', { name: 'Password' });
   }
-  async loginPage() {
-  await this.page.getByRole('link', { name: 'Try it out' }).nth(1).click();
+
+async successfulLogin(username: string, password: string) { 
+    await this.loginPageButton.click(); 
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click(); 
+};
+
+async fileUploadPage() {
+  await this.page.getByRole('link', {name: 'File Upload'}).click();
 }
 }
-
-
-/* steps
-Import
-Define class
-Readonly page: Page
-Constructor
-Method
-Export class
-Import class in test file
-Create instance of class
-Call method in test
-*/
+ 
